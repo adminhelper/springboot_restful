4a9ed5361b7df6571765e25a347a0506e669b8e1
adminhelper <201401339hs@gmail.com>
2021-03-23 17:20:44 +0900

    Structure 및 DispatcherSerlvet 동작 GET POST DELETE  AOP 이용한 Exception Handling Study

---
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.example.restfulwebservice.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+}
---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,54 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController (UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+    //GET /users/1 or /users/10 -> String
+    @GetMapping("/users/{id}")
+    public User retrieveUsers(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not Found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saveUser = service.save(user);
+
+        URI  location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+    }
+}
